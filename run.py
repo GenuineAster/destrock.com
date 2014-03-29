0e8ebb96dbe72ceef6f2ee45f7a6f1478cd4d59f
Mischa-Alff <mischa.alff@gmail.com>
2014-03-29 11:49:44 +0100

    Finished working blog reader

--- run.py
+++ run.py
@@ -3,17 +3,20 @@ from flask.ext.mako import MakoTemplates, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 import json
 import logging
+from kitchen.text.converters import to_unicode
 
 config = json.load(open("config/config.json", "r"))
 
 app = Flask(__name__)
-mako = MakoTemplates(app)
+app.config["MAKO_INPUT_ENCODING"] = 'utf-8'
+app.config["MAKO_OUTPUT_ENCODING"] = 'utf-8'
 app.template_folder = "templates"
+mako = MakoTemplates(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = config.get("database_uri", None)
 db = SQLAlchemy(app)
 
 
-class BlogPost(db.Model):
+class BlogPosts(db.Model):
     id = db.Column(
         db.Integer,
         primary_key=True,
@@ -58,10 +61,39 @@ class BlogPost(db.Model):
 db.create_all()
 
 
+class Post(object):
+    id = None
+    title = None
+    function_type = None
+    function_name = None
+    tags = None
+    content = None
+    return_value = None
+
+    def __init__(self, db_post):
+        self.id = db_post.id
+        self.title = db_post.title
+        self.function_type = db_post.function_type
+        self.function_name = db_post.function_name
+        self.tags = db_post.tags
+        self.content = to_unicode(
+            open(
+                "posts/%s" % db_post.link_to_content
+            ).read()
+        )
+        self.return_value = db_post.return_value
+
+
 @app.route("/")
 def index():
-    return render_template("index.html", blog_posts=BlogPost.query.all())
+    blog_posts = []
+    for post in BlogPosts.query.all():
+        blog_posts.append(
+            to_unicode(render_template("blog_post.html", post=Post(post)))
+        )
+    return render_template("index.html", blog_posts=blog_posts)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, use_reloader=True)
