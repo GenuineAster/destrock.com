d9738db2ec986e03fb310a81d6f9b5fbf2c3b8fe
Mischa Aster Alff <mischa@destrock.com>
2014-05-20 13:25:55 +0200

    Remade templates, blog posts now use UTC

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from blog import db
 
 
 class Post(db.Document):
-    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    created_at = db.DateTimeField(default=datetime.datetime.utcnow, required=True)
     title = db.StringField(max_length=255, required=True)
     slug = db.StringField(max_length=255, required=True)
     body = db.StringField(required=True)
